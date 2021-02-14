8e49b7756ad06a9ee0bb0bfda87cc075308252ac
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-14 21:49:22 +0200

    created register validator

--- utils/registerValidator.js
+++ utils/registerValidator.js
@@ -1,5 +1,16 @@
 const { body } = require('express-validator');
 
 module.exports = [
-    
+    body('email', 'The provided email is not valid.').isEmail().isLength({ min: 3 }),
+    body('fullName', 'Please enter your names.').notEmpty().isAlpha('en-US'),
+    body('password', 'The given password should be at least 3 characters long').isLength({ min: 3 }),
+    body('repeatPassword').custom(customPasswordCheck)
 ]
+
+function customPasswordCheck(repeatPassword, { req }) {
+    if (repeatPassword !== req.body.password) {
+        throw new Error('Passwords are not matching.')
+    }
+
+    return true;
+}
