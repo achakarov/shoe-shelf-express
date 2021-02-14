fd03ef0ba75ec01f53e86201f6122f3524d79228
Aleksandar Chakarov <alexander.chakarov1@gmail.com>
2021-02-14 22:14:19 +0200

    Fixed logic for checking fullName

--- utils/registerMiddlewareValidator.js
+++ utils/registerMiddlewareValidator.js
@@ -2,7 +2,7 @@ const { body } = require('express-validator');
 
 module.exports = [
     body('email', 'The provided email is not valid.').isEmail().isLength({ min: 3 }),
-    body('fullName', 'Please enter your names.').notEmpty().isAlpha('en-US'),
+    body('fullName', 'Please enter your names.').notEmpty(),
     body('password', 'The given password should be at least 3 characters long').isLength({ min: 3 }),
     body('repeatPassword').custom(customPasswordCheck)
 ]
